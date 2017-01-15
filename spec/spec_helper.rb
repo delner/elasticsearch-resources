37d240f8929ace7fc37f62b70ff268df370de2ae
David Elner <david@davidelner.com>
2017-01-14 19:19:28 -0500

    Added: spec_helper setup.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,24 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require 'simplecov'
+require 'pry'
+require 'rspec/collection_matchers'
+
+# Start code coverage
+SimpleCov.start
+
 require "elasticsearch/resources"
+
+# Load support files
+Dir[File.join(File.expand_path('../../spec/support/', __FILE__), '**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
